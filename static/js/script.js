a8040c58cdd4cead424f2a5cb7a1ac1e2fe1caab
oficialzr <49753192+oficialzr@users.noreply.github.com>
2023-03-21 18:19:16 +0300

    Add animation 3 page buttons/Add Jinja

--- static/js/script.js
+++ static/js/script.js
@@ -37,3 +37,31 @@ if (animItems.length > 0){
     }, 300);
 }
 
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function nextSlide(){
+    showSlides(slideIndex += 1);
+}
+
+function previousSlide(){
+    showSlides(slideIndex -= 1);
+}
+
+function showSlides(n){
+    let slides = document.getElementsByClassName("item");
+
+    if (n > slides.length){
+        slideIndex = 1
+    }
+    if (n < 1){
+        slideIndex = slides.length
+    }
+
+    for (let slide of slides){
+        slide.style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block"
+}
